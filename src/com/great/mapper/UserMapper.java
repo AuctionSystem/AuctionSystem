958a130893cbef53ebfd69a6ae27db8a224e48e7
SunMoonStarts <634214953@qq.com>
2018-07-03 11:22:47 +0800

    abc

--- src/com/great/mapper/UserMapper.java
+++ src/com/great/mapper/UserMapper.java
@@ -4,9 +4,9 @@ import com.great.modal.User;
 
 public interface UserMapper {
 
-	public void addUser(User user);      // 增加用户
+	public void addUser(User user);     		// 增加用户
 	
-	public void deleteUser(int userId);  // 删除指定用户
+	public void deleteUser(int userId);  		// 删除指定用户
 	
-	public void changeUserStates(Tb);
+	public void changeUserStates(int userId);   // 更改指定用户状态
 }
